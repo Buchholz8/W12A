e92ff7ae1b9a373882acd93937f03d05294d555b
markb <markbuchholz8@gmail.com>
2023-04-04 20:53:04 -0600

    set up //

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+//i first start by setting up the success and failure functions to do their respective tasks
 function success(response){
     document.body.insertAdjacentHTML(`beforeend` , `<div>
     <img src="${response[`data`][`url`]}"></img>
@@ -8,10 +8,12 @@ function success(response){
 function faliure(error){
 document.body[`innerHTML`] = `Error occured . please refresh the page`
 }
+//i set up the axios.request to grab the url 
 axios.request({
     url: `https://random.dog/woof.json`
 }).then(success).catch(faliure);
+//i made a button function to check for clicks and i made it so it funs the function
 let button = document.querySelectorAll(`.clk_but`)
 for(let i = 0 ; i < button.length ; i++){
-    button[i].addEventListener(`click` , success)
+    button[i].addEventListener(`click` , faliure)
 }
