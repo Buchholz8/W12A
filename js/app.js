d3fb30551dff66dae58821db5e4b2b7ff2977045
markb <markbuchholz8@gmail.com>
2023-04-04 20:49:24 -0600

    finished

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
-function success(response){
 
+function success(response){
+    document.body.insertAdjacentHTML(`beforeend` , `<div>
+    <img src="${response[`data`][`url`]}"></img>
+    <p> this a picture of a random dog </p>
+    </div>`)
 }
 function faliure(error){
-document.body.insertAdjacentHTML(`afterbegin` , `<p>Something went very wrong, please refresh</p>`) 
+document.body[`innerHTML`] = `Error occured . please refresh the page`
 }
 axios.request({
     url: `https://random.dog/woof.json`
 }).then(success).catch(faliure);
 let button = document.querySelectorAll(`.clk_but`)
-for(let i = 0 ; i < button ; i++){
-    button.addEventListener(`click` , success)
+for(let i = 0 ; i < button.length ; i++){
+    button[i].addEventListener(`click` , success)
 }
